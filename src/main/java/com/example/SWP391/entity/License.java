09de590b43d648398c837bdab03b10ac0625a160
phatcanlez <phatcanlez@gmail.com>
2024-09-26 22:14:16 +0700

    update entity

---
+++ src/main/java/com/example/SWP391/entity/License.java
@@ -0,0 +1,39 @@
+package com.example.SWP391.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class License {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    String licenseId;
+
+    Date issueDate;
+
+    @NotBlank(message = "License name is required")
+    String licenseName;
+
+    @NotBlank(message = "License type is required")
+    String licenseType;
+
+    @Size(min = 0, max = 300, message = "Description must be between 0 and 300 characters")
+    String description;
+
+    Order order;
+}
