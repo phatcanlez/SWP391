1c9565b4c448cfa6e69af79bf41da92f8878cb1f
phatcanlez <phatcanlez@gmail.com>
2024-09-28 16:06:27 +0700

    test

--- src/main/java/com/example/SWP391/controller/LicenseAPI.java
+++ src/main/java/com/example/SWP391/controller/LicenseAPI.java
@@ -15,24 +15,24 @@ public class LicenseAPI {
         @Autowired
         private LicenseService licenseService;
 
-        @PostMapping("/api/licence-create")
+        @PostMapping("/api/licence")
         public ResponseEntity createLicense(@Valid @RequestBody License license) {
             License newLicense = licenseService.createLicense(license);
             return ResponseEntity.ok(newLicense);
         }
 
-        @GetMapping("/api/licence-get")
+        @GetMapping("/api/licence")
         public void getAllLicense() {
             // Some code here
         }
 
-        @GetMapping("/api/licence-get/{id}")
+        @GetMapping("/api/licence/{id}")
         public void getLicenseById() {
             // Some code here
         }
 
 
-        @PutMapping("/api/licence-update")
+        @PutMapping("/api/licence")
         public void updateLicense() {
             // Some code here
         }
