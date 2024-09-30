9f328aa2f1aad966547eab6d7a549a139404b699
phatcanlez <phatcanlez@gmail.com>
2024-09-30 08:25:56 +0700

    Revert "crud template"
    This reverts commit 3fd695e9a6d95f84b30fdb8271ba1c8d98e70946.

--- src/components/authen-template/index.jsx
+++ src/components/authen-template/index.jsx
@@ -1,11 +1,9 @@
-import React, { } from 'react'
+import React, {} from 'react'
 import "./index.css"
-function AuthenTemplate({ children, marginBottom }) {
+function AuthenTemplate({children}) {
   return (
-    <div className="authen-template" style={{ marginBottom }}>
-      <h1>KOIKICHI</h1>
-      <h2>Scales of Excellence in Koi Transportation</h2>
-      <div className='authen-template__form'>{children}</div>
+    <div className="authen-template">
+        <div className='authen-template__form'>{children}</div>
     </div>
   );
 }
