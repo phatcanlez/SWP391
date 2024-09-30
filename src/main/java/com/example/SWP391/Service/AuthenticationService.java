f77c575eed22e75edff2449e18e4616baa5dba2e
phatcanlez <phatcanlez@gmail.com>
2024-09-30 08:18:27 +0700

    git revert

--- src/main/java/com/example/SWP391/Service/AuthenticationService.java
+++ src/main/java/com/example/SWP391/Service/AuthenticationService.java
@@ -1,13 +1,14 @@
 package com.example.SWP391.service;
 
-import com.example.SWP391.entity.Customer;
+import com.example.SWP391.entity.Account;
 import com.example.SWP391.exception.DuplicateException;
 import com.example.SWP391.exception.NotFoundException;
-import com.example.SWP391.model.DTO.CustomerResponse;
-import com.example.SWP391.model.DTO.LoginRequest;
-import com.example.SWP391.model.DTO.RegisterRequest;
-import com.example.SWP391.model.DTO.TokenService;
-import com.example.SWP391.repository.CustomerRepository;
+import com.example.SWP391.model.DTO.authenticatonDTO.AccountResponse;
+import com.example.SWP391.model.DTO.authenticatonDTO.LoginRequest;
+import com.example.SWP391.model.DTO.authenticatonDTO.RegisterRequest;
+import com.example.SWP391.model.DTO.authenticatonDTO.TokenService;
+import com.example.SWP391.model.Enum.Role;
+import com.example.SWP391.repository.AccountRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,13 +20,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class AuthenticationService implements UserDetailsService {
+    //xử lý logic, xử lý nghiệp vụ
     @Autowired
-    CustomerRepository customerRepository;
+    AccountRepository accountRepository;
 
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -39,50 +40,56 @@ public class AuthenticationService implements UserDetailsService {
     @Autowired
     TokenService tokenService;
 
-    public CustomerResponse register(RegisterRequest registerRequest){
-        Customer customer = modelMapper.map(registerRequest, Customer.class);
-        try{
-            customer.setStatus(true);
-            String originPass = customer.getPassword();
-            customer.setPassword(passwordEncoder.encode(originPass));
-            Customer newAccount = customerRepository.save(customer);
-            return modelMapper.map(newAccount, CustomerResponse.class);
+    public AccountResponse register(RegisterRequest registerRequest) {
+        Account account = modelMapper.map(registerRequest, Account.class);
+        try {
+            String originPass = account.getPassword();
+            account.setPassword(passwordEncoder.encode(originPass));
+            account.setRole(Role.CUSTOMER);
+            account.setStatus(true);
+            Account newAccount = accountRepository.save(account);
+            return modelMapper.map(newAccount, AccountResponse.class);
         } catch (Exception e) {
-            if(e.getMessage().contains(customer.getEmail()))
-            {
+
+            if (e.getMessage().contains(account.getEmail())) {
                 throw new DuplicateException("Duplicated Email! Created Fail");
-            }
-            else if(e.getMessage().contains(customer.getPhoneNumber())){
-                throw new DuplicateException("Duplicated Phone! Created Fail");
-            }
-            else{
-                throw new DuplicateException("UnExpected Error");
+            } else if (e.getMessage().contains(account.getUsername())) {
+                throw new DuplicateException("Duplicated Username! Created Fail");
+            }else {
+                System.out.println(e.getMessage());
+                throw new DuplicateException(e.getMessage());
             }
 
 
         }
+        // tương tự context.add C#
     }
 
-    public CustomerResponse login(LoginRequest loginRequest){
+    public AccountResponse login(LoginRequest loginRequest) {
         try {
 
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    loginRequest.getEmail(),
+                    loginRequest.getUsername(),
                     loginRequest.getPassword())); //kh có thì catch exception
-            Customer customer = (Customer) authentication.getPrincipal(); //lấy thông tin ng dùng và cast về account
-            CustomerResponse accountResponse = modelMapper.map(customer, CustomerResponse.class);
-            accountResponse.setToken(tokenService.generateToken(customer));
+            Account account = (Account) authentication.getPrincipal(); //lấy thông tin ng dùng và cast về account
+            AccountResponse accountResponse = modelMapper.map(account, AccountResponse.class);
+            accountResponse.setToken(tokenService.generateToken(account));
             return accountResponse;
-        }catch (Exception e){
+        } catch (Exception e) {
             //error => throw new exception
             throw new NotFoundException("Email or Password is invalid!!");
         }
 
     }
 
+    public List<Account> getAllAccounts() {
+        List<Account> list = accountRepository.findAll(); // findAll() lấy tất cả account trong DB
+        return list;
+    }
+
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return customerRepository.findCustomerByEmail(email);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return accountRepository.findByUsername(username);
     }
 }
