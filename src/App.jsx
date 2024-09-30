f76691f1bbc57a33eb9b7f6d050c6943f1f377a2
Nguyen Anh Khoa <khoanase160978@fpt.edu.vn>
2024-09-30 14:46:55 +0700

    service-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
-import HomePage from './pages/home';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login";
+import RegisterPage from "./pages/register";
+import HomePage from "./pages/home";
+import Service from "./pages/service";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage/>,
+      element: <HomePage />,
     },
     {
       path: "login",
-      element: <LoginPage/>,
+      element: <LoginPage />,
     },
     {
       path: "register",
-      element: <RegisterPage/>,
+      element: <RegisterPage />,
+    },
+    {
+      path: "/service",
+      element: <Service />,
     },
   ]);
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/service/international/index.jsx
@@ -0,0 +1,209 @@
+import "./index.scss";
+
+function International() {
+  const container1P1 = { fontSize: 45, fontWeight: 700 };
+  const container1P2 = { fontSize: 30, fontWeight: 300 };
+  const container1P3 = { fontSize: 25, fontWeight: 600 };
+  const container1P4 = { fontSize: 25, fontWeight: 300 };
+  const container2P1 = { fontSize: 50, fontWeight: 600 };
+  const container2P2 = { fontSize: 32, fontWeight: 500 };
+  return (
+    <div className="international">
+      <div className="international__container1">
+        <div className="international__container1__title">
+          <p style={container1P1}>
+            WHY SHOULD YOU EXPAND YOUR BUSINESS TO THE VIETNAMESE MARKET?
+          </p>
+          <p style={container1P2}>
+            Explore the potential of this promising market!
+          </p>
+        </div>
+        <div className="international__container1__diagram">
+          <img
+            src="https://s3-alpha-sig.figma.com/img/3ada/3a94/0043925d01cdb2916aa9e9d639d44488?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nOWahD5QWemdhEgschcUCBUS5iJI7huzF217mEHUERYBpg54y-nmB6pqbotCbw7Yh3iV38-~zo3CGyUTtK8LLx~lroJ6R3e00TNGxKTD-qGN41uD-3Ihx4aHOlvbU-uAFZEYWtMMSvHXIlnKUOIsXwQtnZqXmg~BBG-OkTBtDEUl7Ny0M~lnl9ge81eGynutQ8uxCSCdwMqYtUJ-USIjH3RNrQpNL3M-WCtNzwCXHvvg8hIHO91kdJ3w5BWYd5IWKUJY9t2bFxw4nFyDdS2R3QBuqJn2x1RjZT2QKvOz9EJ8zU60mRoVrHk3tJdhunmaYLdGTncyvVIda2kY~DavaA__"
+            alt=""
+          />
+          <ul>
+            <li>
+              <img
+                src="https://s3-alpha-sig.figma.com/img/ae24/e9c2/4326c6bb6cd180178f349e8759e5fb0d?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NrM~0MeSr-KBJ6GBq62940x1~mFjg6vArlZic2i09hXCnAOTA-n9XtqBmshwnXNdunSK7LHwkL8EEbUCOS1tgt18Tqfb4beBdcOvolCQ751Jp0MjtQPsEEDw9A3sb8XKECnXhTAkY-V4pVsckFyYMgmxb5oGZje50leRqT1FTJ9zmpeoeiKGBvIgNf9vyUSFDZoqPtiZ2O0pjsWycS4msVy-FkWy~fZsshyHooykq4K4fcOYDuneveI0j8OCQg6oKsygl-r2oIs73XGInx5TeVBvWqWi1hgH~0mtbXM4Fhm5VL14QYYiEqoYdtprAeOT7nrbkNMWrLp~CMp77Ao4VQ__"
+                alt=""
+              ></img>
+              <p>B2c Revenue</p>
+            </li>
+            <li>
+              <img
+                src="https://s3-alpha-sig.figma.com/img/5c3c/2f98/67b6f573ffecaea72c93868494e2ad4d?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hkp0LxZsnnrqW1kY~v5uHlvwp~DsDPQPMs5q99iJphIfPcWh9A8OLcx9Sa7yKrTqP4br4miGL7sC~6PhITLG1x2qptc1angE0WDW6OuG1ofcBVWXrXISK7qi6MX6MVNRL7kIC1-OxlWvnq3Qn0qnpF3cEJhEa28J6UTGK59CiT~4FJOa0nysPwCJRuz6kIUF1I9WITRbVRxMCCKB80g0b--xhJ-t-atN0TFwPm1zjOKcptL~wEGTcCECeXtmeTnO04EeIombVKLLBdzxU48n2GP4nPuiSbSNX6o~LnALrGBWA835oc5Q7caSihjADy24nfIlHWmsgbmkJH2fIBQA0g__"
+                alt=""
+              ></img>
+              <p>Growth rate</p>
+            </li>
+          </ul>
+        </div>
+        <div className="international__container1__foot">
+          <p style={container1P3}>
+            B2C e-commerce revenue in Vietnam from 2018 to 2023 (billion USD){" "}
+          </p>
+          <p style={container1P4}>Source: VnEconomy</p>
+        </div>
+      </div>
+      <div className="international__container2">
+        <div className="international__container2__left">
+          <p style={container2P1}>CHALLENGES WHEN SELLING IN VIETNAM</p>
+          <p style={container2P2}>
+            Vietnam is a potential e-commerce market. However, there are many
+            obstacles that Japanese sellers have to cope with.
+          </p>
+        </div>
+        <div className="international__container2__right">
+          <p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/4020/ceef/37dac013b794e671e50240087216e969?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PpIRxRVp-h~XYF7inxHmB~MVm1X4ClCzOj12eyWZlXsMlAG4NVNdRMG3WkRtHpNTJCKUVOZUyIWbdVJT5IkgiKewxpj9WWXUAppXyI~61~~KxInuUjZucU5nFjissCcR6ec6dr7czSfX0ROhmReeSJbXHYDUrONsz6TSrLa5LXcgylTy8QkrkWXKcYN2qZiaw4zHpl4~8dQVl-war1mI8GnDquOlb2ruDo7sHoNX5aZEhvhuo~U1TnV7g1QNvNP3uXb-VyjH00axIGzgE6X33zuNdeIQ2q-397XPOcd081HWHAIQd~9h0lUcwJIaEO3DJHF3IezbSDMqfNlSLv8a2Q__"
+              alt=""
+            />
+            Language barier
+          </p>
+          <p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/a83b/8d1a/33983defd6097c8dd9f0c0fa506405fe?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HEbcqJPiTSWg8BPdOX8vF3~8itMnfUDXb5J5~flZD2ad1x9wYZMY8FVMZzrWEfkBexmikiq6G5T4kfE9OEPFbvNF7IcCby0kX92OQVXNdFjfovxvmfmZNLwyCillFN64kiT2dOiwTcdq1xn6W34IqI51~RWBLYZFqAMDCKmJV2F9bIxGFk7lVyjMQyPyPUvEVYX8ZZS5GzvcXEKrjlG9xf8kOXNGbLNbCG8mz2TtkTDUj9jGMAd8F4hUMexuq~zcOxjx0yTys05BO2s5jdRlfAJSVYvgCBfDaw2CJS7wl~XJ6mQWydB9eS8QiYEpLwpi6lNLmQGA~bICc9ztFhs3iQ__"
+              alt=""
+            />
+            Cultural differences
+          </p>
+          <p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/5408/49f5/3a95f11b337bef76a828df3fa2e4f9f8?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pkodD6XaBfWC0v15QAhVectooOOf21kuN-ZUQ71~oFiV7dXWQVq9Oyq-AXUhWI6QfgYeW1fSH6C34pnVTsmx2-0faE1lb0W4uvZLNpj2VOUBRwlQ6Geb~kKLNDXIczqlJKSWalWUcQeV3Ckvw~aoXr3SCmafYOW-n8fFvGUOkGGQmCpC3KTZC3oqvMDxtMXodYZB2~p2Dx09dsceV19qJI7xctXFRMEOhN0X13YxUdY0cSb7xKhOiYz7qJp0ApT5KpbGlu8k~EKNrZTbsN5sCaTCVMwDTNe47wEoNptu~n9bb6AUkDGkSoPsz-LETgr9If2pp0LLtzAnaxkKEhENTw__"
+              alt=""
+            />
+            Complex regulations and laws
+          </p>
+          <p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/ba16/b949/a628b53ef6d834c7f12291321633279a?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Pgb09SihJGUFvSDvdl-EQxtDMP6rH2LzSsW~EijGLFtPP4Y3OeYS8uvS-WydL-jyerxkjWzJE3Y5OIANahxGRBfelZNzwJgUiBSfwwRk~Pq0lmBLqhe5enWEmJ8uOX9rdLSMfm3HYtfJ5jfuqRcMGpiafuS1Zu2jnCO3euV4oPGQfJSkk2xbaqEyCE5INlh2fv1ETHXmRDYaK918XzlfWB-DW9kCaVyb7Vdr5F1gVHFJpGfUY2ojj~vxLguphqhBiME-g3MiaY87C2zlvjFTiGv3XtZ7mfhkhXt31QJ2rpxX4oDn72BiYhMek5iYR6jx~9BZZzmRU6QouaAk5coccw__"
+              alt=""
+            />
+            Order processing and warehousing
+          </p>
+          <p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/b57a/6c42/dce771138dd2a91410d319b0c44df4de?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=b3fEhEppFbI9avXtHAddd5vNZPr4vX--s5wCj9352sBSAj8mq1K7BTQIAth1MWEtYZK2qslgwo0Fltl-ZP71oQh9GL008v3kqiYvULZRJpAtsjbwJKRylX-SDCKZ-NTSlnK5IV11LrEpuTN~V4GRtzFDF8irLdKtr9r0tVKayaQ1yQjrfRqbiNGq9xWInQP3GktXGyXQ8v01D5KAEkydK83Phdxl7QAY88YBY7v7Rlgs-wl~s~CELghglWivzXedlXcG~oMWMIRRdU~oBaKYy7mtNrB3fePzCUU~-YL26MDh5ZBAuqBX9TJX7I9ptdBWjdTH11f3JIFUztVFBXdFjw__"
+              alt=""
+            />
+            Delivery and return processing
+          </p>
+          <p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/b96a/6b59/b9c651f108804535458ad3f41dfffee2?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=B3VL5hGvAoY5au8nc7uTBNuDkck2l5EZNwomfqEccmqNgg1nb4E1FWWqggNJfdPPjJNE-MFs47ILzuAISaJ18RQHxDgTznlqYYQnHTkWACB96ZxIMypOHJfk2ihol6cyPfkKq7JnhWq3N6Y8sWOypAhPv4o-ttjWPwEa4gsHRNFLi0YObtbUbTMEkaDvY3g7mT-5FYWG7uaoZDQA0YbnB8AIxAgYzuscumi4FqRZVsGu-xP3FXpjPSiDTEo0-145mfzsU6DR~EyOkeqmSzhq28OmesnF6jAqM14C8NMNWU-0b-QWudJr7KwSaPJJF9lBM6X2lLzpIEjY9rXZG2NAoQ__"
+              alt=""
+            />
+            Customer care
+          </p>
+        </div>
+      </div>
+      <div className="international__container3">
+        <p className="international__container3__title">
+          How does KOIKICHI LOGISTIC help Japanese businesses when entering the
+          Vietnamese market?
+        </p>
+        <div className="international__container3__rectangle">
+          <div className="international__container3__rectangle__1">
+            <p className="international__container3__rectangle__title">
+              Register business in Vietnam
+            </p>
+            <p className="international__container3__rectangle__paragraph">
+              We help foreign sellers register their business in Vietnam thanks
+              to our partnership with the Department of Planning and Investment
+              in major cities such as Hanoi, Da Nang and Ho Chi Minh City.
+            </p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/c7af/6a0a/5500e104f5c724c263645a0d2bdf237b?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=I8mMbrWx7e-HBcL9v0kxYQQxlmXwluyrtakEUczXndaNGSAIYZmB01b0wr3lDZCB~pcGqOsMlIvzpxnuDWJSWSe3qNNf1LEgW3chst4pdPqqIVCkP91DmGcx9xMP2-6zAHBo3SL9SGSVky0kyRwtuX~zYPknZZZZIySjOeVF-66DgUebIYf8CoDeYc2~Gmjf64u2mUQrFB94SR5qPg6c8eUj0VwtOsEjbuEYoK9jg6~wXuA-izk-gJnSEX3CkrjLtPIcw9HNfRVLoQ01mqqa5RxTIsrbpxB0VEq1fPAu4fD4PCWoO9~SOTl-wBuWhRgJbPpG6I9CltA0iPbv7hnLxw__"
+              alt=""
+              className="international__container3__rectangle__image"
+            />
+          </div>
+          <div className="international__container3__rectangle__2">
+            <p className="international__container3__rectangle__title">
+              Open a multi-platform online store in Vietnam
+            </p>
+            <p className="international__container3__rectangle__paragraph">
+              We support foreign merchants to set up online stores on various
+              platforms and channels, including their own website, social
+              networks, Shopee, Lazada, etc. This multi-channel approach helps
+              increase brand coverage in the Vietnamese market.
+            </p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/ff29/3b42/c17ff59eef38b0f422a41d95c9cbd97c?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ea3FeyibuHPpBGaMGFazDrkvxs2jq8ae3tCESpolTTQOnr56JUwGRb70t6Xd2QA9~37CUajD7ysf37GDLh14-mWUer8IokaBA~PV6s-ZiHw1ucUwpKqoDRlXJJbcAJu3EaNQLhwLD9tB69rdYEw2Bb-y-KpsLgi1rb5eJ7R9-tH6XntXBw1cCssn46gKWjoNpcBiNxrVSuYieQU38r0FwCLZJ7tJ1DGNZWdoggHCV9J~cKecrZk5llAbtE5zYWnHBSbhdPdeGEBVruljBWWdL6XkdQqdQ2M1IOPQ2pgZPwjp81hVZ2d27Qkq8GiMPB~ioPDST0FUGCqjYGYsSVmfsw__"
+              alt=""
+              className="international__container3__rectangle__image"
+            />
+          </div>
+          <div className="international__container3__rectangle__3">
+            <p className="international__container3__rectangle__title">
+              Providing goods storage and handling services
+            </p>
+            <p className="international__container3__rectangle__paragraph">
+              KOIKICHI Logistic satisfies all your requirements for warehousing
+              and order fulfillment in Vietnam. Providing an OMS system to help
+              sellers manage inventory and handle import/export of goods.
+            </p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/06e0/cee2/cf35d4ef2435eb5754aab0504939fc8c?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ehec58RxTSeFHHSFg6gnnJD6QHzft5LI-f2G7jZ09vEfjoH-mExdk9ZIkVRm1KYEyppS~KRlpvQgPTfmuGHOu2VJO5C4sJQjOLXCldCdRGKMdZbzqjJcHNA0XszZfZeFXvP5X0zi~OOQV4yHUBdlhOJcPfs-At36K6Vlufr~3lN1twosiF96sIuxT-jjyt8iInIeAjM4lMZPENOlgXMlOpg0qdsSV20QbBREhQgfH5DmM0AcKden0Yozn3-0r0-wnLCoMe2i4UeRjF3eYmSIeqWhCgtBJSKE1yvUZMnbq4~fUWN-cFJ6mQoaHnBEo9Uc0Vv0l8eQhmICY2hzxavebA__"
+              alt=""
+              className="international__container3__rectangle__image"
+            />
+          </div>
+        </div>
+        <div className="international__container3__rectangle">
+          <div className="international__container3__rectangle__4">
+            <p className="international__container3__rectangle__title">
+              Marketing support in Vietnamese market
+            </p>
+            <p className="international__container3__rectangle__paragraph">
+              KOIKICHI provides appropriate marketing services for foreign
+              sellers such as running ads, managing fanpages, livestreaming
+              sales, creating SEO strategies,... to improve product visibility
+              and increase sales. .
+            </p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/b754/c3fd/3db5320d71276a17f10e802a167deaa8?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iU76QPhkTZ1Oa8D5eWoNXQ9Mcz8Pevz1FYJ-cp69b4i2S1lairlR6JTL8sdzF~CIslEcZSqZS38C6TgO6pFQccQqZuHOFrKCok2DtnrxN-a0NRz8v-Chl915DYCr7QVBlK3xTfVm4aBdiZnCVHVHWCjYOtdBTfhVLmJgGFhMqvce7JaAyQXoddgECK-F8I5-xKMETuONsnbTJK8H0wt-NAavstV5D~Zu1aw5X18kT2hpyLQcqCTZocoHRg0bloqKRGFkUMyYGza9X~eZsHZSbsUJx1KWl-B2gyBqmLQdxfyzc3QLDVF00ZJCQqO7cwD4s1ds~zXROMYLbNkkaakiiQ__"
+              alt=""
+              className="international__container3__rectangle__image"
+            />
+          </div>
+          <div className="international__container3__rectangle__5">
+            <p className="international__container3__rectangle__title">
+              After-sale customer care
+            </p>
+            <p className="international__container3__rectangle__paragraph">
+              Rest assured that whether you are in Vietnam or not, the KOIKICHI
+              Logistic team is always ready to take care of customers on your
+              behalf.
+            </p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/952b/82ad/5db9f7c6fbab8520072019b71dfc9b39?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PRdvRK~HOELYAK9qaNWQbsJs~5y7ZricrVINXS0Kbc5pR2p4pvLCgJu9woXCy0bFJBfC~0Vtzgh82z7-aSsy9KIFJF4oGDb6IfxAaaZl7Puwgjj2FSE6dFjUkJxeYM7KyHwapUAKwhPae2M5V-KpwXPBAt8zaaHtzI4WrfKlW9EqXHPsk0bV86UcVwDzBh5Bx2Ox197cllVx9-GUlkTuteD0tg62vBfIOzIEn0VyZzH~qd~w9WQhm9JRugvTZ0UGfdfaAUdvHX9mXCMxUwasQrR5p-RemNHN2qh71XNWfgNS6hezC-O3Urk0EUlwRdMvk7rbmUjjmWHplzHf0hPGsw__"
+              alt=""
+              className="international__container3__rectangle__image"
+            />
+          </div>
+          <div className="international__container3__rectangle__6">
+            <p className="international__container3__rectangle__title">
+              Support revenue and expenditure management/reconciliation
+            </p>
+            <p className="international__container3__rectangle__paragraph">
+              KOIKICHI simplifies expense tracking and reconciliation for
+              foreign businesses, making the management of financial
+              transactions more transparent and easier.
+            </p>
+            <img
+              src="https://s3-alpha-sig.figma.com/img/b4ca/2f17/e5b2a60a9f4876566f73835f2bd2ed01?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lKMVm6dCx4he12kg0vnPIOA9DkMEQoEf5LmG0plg7Y2moS38nSYQRQYPmhWw9rglKe4ex4BMNHg1cknfdRVEd6yt-Q5wsvuxbweP1ZMj1s8gW3c~gskLBKeruLYmqxMOvPmNggdAc3E7EAZ5Dw1MhCZzqyOYDT3kddnpoZcxdoaHPj59-LdSu~6Mmf4bfW8LVCS9ybE8zk80AiFBRi68Ew7nanhQqbMbhXm3m24REi6i70tstgTzrDdj4kCyf0UOqPW-J3A~s2LbUwn6uaZJPxoLYM-qzzCZ1PNlhNE~xTPdR7I5oerU9MTOKAEHzZPtx62ENgxTHFjFyj3tCynumA__"
+              alt=""
+              className="international__container3__rectangle__image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default International;
