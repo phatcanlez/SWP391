83a48ec5c531e89e39daaddfd936415c557f4fae
quoc10973 <rumvle01667057271@gmail.com>
2024-10-01 01:35:23 +0700

    email sender

---
+++ src/main/java/com/example/SWP391/model/DTO/EmailDetail.java
@@ -0,0 +1,12 @@
+package com.example.SWP391.model.DTO;
+
+import com.example.SWP391.entity.Account;
+import lombok.Data;
+
+@Data
+public class EmailDetail {
+    Account receiver;
+    String subject;
+    String link;
+
+}
