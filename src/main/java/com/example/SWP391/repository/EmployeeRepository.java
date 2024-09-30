29b81d65844c551e345c5b7932b9ecb2c1c5d5e6
quoc10973 <rumvle01667057271@gmail.com>
2024-09-30 14:50:00 +0700

    modify

--- src/main/java/com/example/SWP391/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SWP391.repository;
-
-
-import com.example.SWP391.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-    Employee findEmployeeById(String id);
-}
