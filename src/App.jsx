5896303d15b4d9575a1b2d0736cc2e8ad3c93bd6
Truc Phuong <maitrucphuong0111@gmail.com>
2024-09-30 11:51:06 +0700

    crud-template

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from './pages/login';
 import RegisterPage from './pages/register';
-import HomePage from './pages/home';
+import HomePage from './pages/home';            
+import AboutUs from './pages/about';
+import ManageOrder from './pages/admin/manage-order';
+import Dashboard from './components/dashboard';
 
 function App() {
   const router = createBrowserRouter([
@@ -17,6 +20,20 @@ function App() {
     {
       path: "register",
       element: <RegisterPage/>,
+    },
+    {
+      path: "about",
+      element: <AboutUs/>,
+    },
+    {
+      path: "dashboard",
+      element: <Dashboard/>,
+      children: [
+        {
+          path: "customer",
+          element: <ManageOrder/>
+        }
+      ]
     },
   ]);
   return (
