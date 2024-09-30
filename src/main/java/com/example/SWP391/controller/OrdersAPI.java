ee0359ea78a9b64eac077d8676c98ab1078961c4
quoc10973 <rumble01667057271@gmail.com>
2024-10-01 06:17:35 +0700

    OrdersAPI

---
+++ src/main/java/com/example/SWP391/controller/OrdersAPI.java
@@ -0,0 +1,42 @@
+package com.example.SWP391.controller;
+
+import com.example.SWP391.entity.Orders;
+import com.example.SWP391.service.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin("*")
+public class OrdersAPI {
+
+        @Autowired
+        private OrderService orderService;
+
+        @PostMapping("/api/orders")
+        public ResponseEntity createLicense(@Valid @RequestBody Orders orders) {
+            return ResponseEntity.ok(orderService.createOrder(orders));
+        }
+
+        @GetMapping("/api/orders")
+        public ResponseEntity getAllOrders() {
+                List<Orders> list = orderService.getAllOrders();
+                return ResponseEntity.ok(list);
+        }
+
+        @GetMapping("/api/orders/{id}")
+        public ResponseEntity getLicenseById(@PathVariable long id) {
+            return ResponseEntity.ok(orderService.viewOrderById(id));
+        }
+
+
+        @PutMapping("/api/orders")
+        public ResponseEntity updateLicense(@RequestBody @Valid Orders order, long id) {
+            return ResponseEntity.ok(orderService.updateOrder(order, id));
+        }
+
+}
