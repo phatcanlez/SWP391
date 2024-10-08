94675b258735e928aeef9d911101c3d222f789fa
phatcanlez <phatcanlez@gmail.com>
2024-10-08 22:53:57 +0700

    add

--- src/main/java/com/example/SWP391/repository/OrderRepository.java
+++ src/main/java/com/example/SWP391/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.example.SWP391.repository;
 
+import com.example.SWP391.entity.Account;
 import com.example.SWP391.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,5 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     Orders findByorderID(String id);
+    List<Orders> findByAccount(Account account);
 }
--- src/main/java/com/example/SWP391/repository/StatusRepository.java
+++ src/main/java/com/example/SWP391/repository/StatusRepository.java
@@ -2,8 +2,12 @@ package com.example.SWP391.repository;
 
 import com.example.SWP391.entity.Orders;
 import com.example.SWP391.entity.Status;
+import com.example.SWP391.model.Enum.StatusInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface StatusRepository extends JpaRepository<Status, Long> {
     Status findById(long id);
+    List<Status> findByStatusInfo(StatusInfo statusInfo);
 }
