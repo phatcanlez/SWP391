5f02f4381988213175e45dda340e9cff7e6fc4d9
quoc10973 <rumble01667057271@gmail.com>
2024-10-10 13:51:38 +0700

    add code

--- src/main/java/com/example/SWP391/controller/AuthenticationAPI.java
+++ src/main/java/com/example/SWP391/controller/AuthenticationAPI.java
@@ -4,15 +4,19 @@ import com.example.SWP391.entity.Account;
 import com.example.SWP391.model.DTO.authenticatonDTO.AccountResponse;
 import com.example.SWP391.model.DTO.authenticatonDTO.LoginRequest;
 import com.example.SWP391.model.DTO.authenticatonDTO.RegisterRequest;
+import com.example.SWP391.model.DTO.forgotPassword.ForgotPasswordRequest;
+import com.example.SWP391.model.DTO.forgotPassword.ResetPasswordRequest;
 import com.example.SWP391.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @CrossOrigin("*") //cho phép tất cả code truy cập
 @SecurityRequirement(name = "api") //bắt buộc có, nên nhớ
@@ -20,6 +24,7 @@ public class AuthenticationAPI {
 
     @Autowired
     AuthenticationService authenticationService;   //tương tự new package: AuthenticationService a = new AuthenticationService();
+
     @PostMapping("/api/register")
     public ResponseEntity register(@RequestBody @Valid RegisterRequest registerRequest) {
         //api nhận request và object từ FE, sau đó nhờ service xử lý, thông qua lớp authenticationService
@@ -32,6 +37,7 @@ public class AuthenticationAPI {
     @PostMapping("/api/login")
     public ResponseEntity login(@RequestBody LoginRequest loginRequest)
     {
+        log.info("loginAPI");
         AccountResponse a = authenticationService.login(loginRequest);
         return ResponseEntity.ok(a);
     }
@@ -42,5 +48,19 @@ public class AuthenticationAPI {
         List<Account> list = authenticationService.getAllAccounts();
         return ResponseEntity.ok(list);
     }
+
+    @PostMapping("/api/forgot-password")
+    public ResponseEntity forgotPassword(@RequestBody @Valid ForgotPasswordRequest forgotPasswordRequest) {
+        authenticationService.forgotPassword(forgotPasswordRequest);
+        String notification = "Email sent";
+        return ResponseEntity.ok(notification);
+    }
+
+//    @PostMapping("/api/reset-password")
+//    public ResponseEntity resetPassword(@RequestBody ResetPasswordRequest request) {
+//        authenticationService.resetPassword(request);
+//        String notification = "Password reset successfully";
+//        return ResponseEntity.ok(notification);
+//    }
 
 }
